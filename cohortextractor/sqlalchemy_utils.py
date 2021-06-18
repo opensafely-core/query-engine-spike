cdad5086b5c27df86269785511309dbe515656f3
David Evans <dave.evans@thedatalab.org>
2021-06-18 15:20:05 +0100

    wip

--- cohortextractor/sqlalchemy_utils.py
+++ cohortextractor/sqlalchemy_utils.py
@@ -2,6 +2,10 @@ import sqlalchemy
 
 
 def make_table_expression(table_name, columns):
+    """
+    Return a SQLAlchemy object representing a table with the given name and
+    columns
+    """
     return sqlalchemy.Table(
         table_name,
         sqlalchemy.MetaData(),
@@ -10,6 +14,9 @@ def make_table_expression(table_name, columns):
 
 
 def get_joined_tables(query):
+    """
+    Given a query object return a list of all tables referenced
+    """
     tables = []
     from_exprs = list(query.froms)
     while from_exprs:
@@ -18,4 +25,14 @@ def get_joined_tables(query):
             from_exprs.extend([next_expr.left, next_expr.right])
         else:
             tables.append(next_expr)
+    # The above algorithm produces tables in right to left order, but it makes
+    # more sense to return them as left to right
+    tables.reverse()
     return tables
+
+
+def get_primary_table(query):
+    """
+    Return the left-most table referenced in the query
+    """
+    return get_joined_tables(query)[0]
